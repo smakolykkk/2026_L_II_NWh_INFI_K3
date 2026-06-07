8c2bdda091638a09d86ac3b654b68a14aa3196db
vdi-terminal <vdi-terminal@EDU-WSB.local>
2026-06-07 10:34:01 +0200

    Debugging

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -1,4 +1,3 @@
-
 PLAIN = "plain"
 PLAIN_UP = "plain_uppercase"
 PLAIN_LO = "plain_lowercase"
@@ -9,6 +8,7 @@ SUPPORTED = [PLAIN, PLAIN_UP, PLAIN_LO, JSON]
 
 def get_formatted(msg, imie, format):
     result = ""
+
     if format == PLAIN:
         result = plain_text(msg, imie)
     elif format == PLAIN_UP:
@@ -17,12 +17,12 @@ def get_formatted(msg, imie, format):
         result = plain_text_lower_case(msg, imie)
     elif format == JSON:
         result = format_to_json(msg, imie)
+
     return result
 
 
 def format_to_json(msg, imie):
-    return ('{ "imie":"' + imie + '", "mgs":' +
-            msg + '"}')
+    return '{ "imie":"' + imie + '", "msg":"' + msg + '"}'
 
 
 def plain_text(msg, imie):
