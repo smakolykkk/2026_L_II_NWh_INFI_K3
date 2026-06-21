c1616a1b1c3dffeed0c8ec8892e77d574ad0cbe6
vdi-terminal <vdi-terminal@EDU-WSB.local>
2026-06-21 09:56:13 +0200

    Błąd w formater

--- hello_world/formater.py
+++ hello_world/formater.py
@@ -25,7 +25,7 @@ def format_to_json(msg, imie):
     return '{ "imie":"' + imie + '", "msg":"' + msg + '"}'
 
 
-def plain_text(msg, imie):
+def plain_text(msg, imie):"""""""""""
     return imie + ' ' + msg
 
 
